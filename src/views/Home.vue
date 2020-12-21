5f89c4acca92f0bc5145a1642f2c853314128dfb
massndp <bayu8989.88@gmail.com>
2020-12-21 09:43:10 +0700

    Upload project Vue

---
+++ src/views/Home.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="home">
+  <HeaderSunshop/>
+  <HeroSunshop/>
+  <Woman/>
+  <Instagram/>
+  <Partners/>
+  <Footer/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import HeaderSunshop from '@/components/HeaderSunshop.vue';
+import HeroSunshop from '@/components/HeroSunshop.vue';
+import Woman from '@/components/Woman.vue';
+import Instagram from '@/components/Instagram.vue';
+import Partners from '@/components/Partners.vue';
+import Footer from '@/components/Footer.vue';
+
+
+
+
+export default {
+  name: 'Home',
+  components: {
+    HeaderSunshop,
+    HeroSunshop,
+    Woman,
+    Instagram,
+    Partners,
+    Footer
+}
+}
+</script>
